c9ebf932511cc88cc7149306f8dbf8aa0303cd3f
Joseph Beltrami <jpbeltrami@gmail.com>
2019-10-06 22:47:45 +0800

    alignment

--- src/components/Main.js
+++ src/components/Main.js
@@ -31,21 +31,60 @@ export default () => {
 
   return (
     <Main>
-      <div>
-        <div>progess bar</div>
-        <h1></h1>
-      </div>
+      <Progress max={30} value={10}></Progress>
+      <Word>星期</Word>
+      <Answers>
+        <AnswerButton>week; CL:個|个[ge4]</AnswerButton>
+        <AnswerButton>time; length of time; moment; period</AnswerButton>
+        <AnswerButton>time; length of time; moment; period</AnswerButton>
+        <AnswerButton>computer; CL:臺|台[tai2]</AnswerButton>
+      </Answers>
     </Main>
   )
 }
 
+// const Main = styled.div`
+//   min-height: calc(100vh - 100px);
+//   width: 100vw;
+//   display: flex;
+//   justify-content: center;
+//   padding-top: 40px;
+//   box-sizing: border-box;
+//   overflow: hidden;
+//   overflow-y: scroll; 
+// `;
+
 const Main = styled.div`
-  min-height: calc(100vh - 100px);
-  width: 100vw;
+  display: grid;
+  grid-template: 100px 300px auto / 40px 40px 80px auto 80px 40px 40px;
+  grid-template-areas: 
+  ". p p p p p ."
+  ". . . w . . ."
+  ". . a a a . .";
+`;
+
+const Progress = styled.progress`
+  grid-area: p;
+  justify-self: center;
+  align-self: center;
+`;
+
+const Word = styled.h1`
+  grid-area: w;
+  justify-self: center;
+  align-self: center;
+`;
+
+const Answers = styled.div`
+  grid-area: a;
+  justify-self: center;
+  align-self: center;
   display: flex;
   justify-content: center;
-  padding-top: 40px;
-  box-sizing: border-box;
-  overflow: hidden;
-  overflow-y: scroll; 
+  flex-wrap: wrap;
+  align-items: center;
+`
+
+const AnswerButton = styled.button`
+  width: 50%;
 `;
